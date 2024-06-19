4ffe513f36012638e106e213001a0eaf2df9e278
it985 <62421120+it985@users.noreply.github.com>
2024-06-19 17:03:52 +0800

    Add files via upload
    自定义样式js

---
+++ public/js/coustom.js
@@ -0,0 +1,15 @@
+// 这里编写自定义js脚本；将被静态引入到页面中
+var OriginTitile = document.title;
+var jiao;
+
+document.addEventListener('visibilitychange', function () {
+		if (document.visibilityState === undefined || document.visibilityState === 'visible') {
+				document.title = '(^∇^*)欢迎肥来！' + OriginTitile;
+				jiao = setTimeout(function () {
+						document.title = OriginTitile;
+				}, 4000);
+		} else {
+				document.title = '你怎么跑啦w(ﾟДﾟ)w';
+				clearTimeout(jiao);
+		}
+});
